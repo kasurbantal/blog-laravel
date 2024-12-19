2f944ed5a06d83aa968d544040bfcd2e422a85e5
kasurbantal <kasurbantal880@gmail.com>
2024-12-19 21:56:18 +0700

    feat: display posts data dynamically from Route in posts.blade.php

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,25 +1,17 @@
 <x-layout>
 <x-slot:title>{{$title}}</x-slot:title>
 
-    <article class="py-8 max-w-screen-md border-b border-gray-300">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Artikel 1</h2>
-        <div class="text-base text-gray-500">
-            <a href="#">Reza Maulana Ismail</a> | 18 Desember 2024
-        </div>
-        <p class="my-4 font-light">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, corrupti excepturi dolores non doloribus dignissimos facere in, maxime voluptates fuga ea nobis praesentium maiores. Minus cum harum facilis velit reprehenderit.
-        </p>
-        <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
-    </article>
-    <article class="py-8 max-w-screen-md border-b border-gray-300">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Artikel 2</h2>
-        <div class="text-base text-gray-500">
-            <a href="#">Reza Maulana Ismail</a> | 19 Desember 2024
-        </div>
-        <p class="my-4 font-light">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque magni minus eius non nemo dolores sit, vel et repellendus repudiandae vitae recusandae explicabo eum ullam cupiditate laboriosam. Repudiandae, molestias consequuntur.
-        </p>
-        <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
-    </article>
-
+{{-- array posts berasal dari web.php, sedangkan $post adalah variabel baru yang dibuat sendiri di halaman ini --}}
+@foreach ($posts as $post)
+<article class="py-8 max-w-screen-md border-b border-gray-300">
+    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title']}}</h2>
+    <div class="text-base text-gray-500">
+        <a href="#">{{ $post['author']}}</a> | 18 Desember 2024
+    </div>
+    <p class="my-4 font-light">
+        {{ $post['body']}}
+    </p>
+    <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+</article>
+@endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,18 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog']);
+    return view('posts', ['title' => 'Blog', 'posts' =>[
+        [
+            'title' => 'Artikel 1',
+            'author' => 'Reza Maulana Ismail',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, corrupti excepturi dolores non doloribus dignissimos facere in, maxime voluptates fuga ea nobis praesentium maiores. Minus cum harum facilis velit reprehenderit.'
+        ],
+        [
+            'title' => 'Artikel 2',
+            'author' => 'Reza Maulana Ismail',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque magni minus eius non nemo dolores sit, vel et repellendus repudiandae vitae recusandae explicabo eum ullam cupiditate laboriosam. Repudiandae, molestias consequuntur.'
+        ]
+    ]]);
 });
 
 Route::get('/contact', function () {
