b994cb37577d4241fdbb1d513de816f905ae3407
kasurbantal <kasurbantal880@gmail.com>
2024-12-19 21:31:23 +0700

    feat: make article UI in blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,25 @@
 <x-layout>
 <x-slot:title>{{$title}}</x-slot:title>
-  <h3 class="text-xl">Welcome to my Blog!</h3>
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Artikel 1</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Reza Maulana Ismail</a> | 18 Desember 2024
+        </div>
+        <p class="my-4 font-light">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, corrupti excepturi dolores non doloribus dignissimos facere in, maxime voluptates fuga ea nobis praesentium maiores. Minus cum harum facilis velit reprehenderit.
+        </p>
+        <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Artikel 2</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Reza Maulana Ismail</a> | 19 Desember 2024
+        </div>
+        <p class="my-4 font-light">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque magni minus eius non nemo dolores sit, vel et repellendus repudiandae vitae recusandae explicabo eum ullam cupiditate laboriosam. Repudiandae, molestias consequuntur.
+        </p>
+        <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </article>
+
 </x-layout>
