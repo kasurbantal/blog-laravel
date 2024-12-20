598470f5e5c851c7aa0e9676d7c829131e225806
kasurbantal <kasurbantal880@gmail.com>
2024-12-20 22:08:59 +0700

    refactor: move Post class to App\Models with proper namespacing

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,7 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-
-class Post {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'artikel-1',
-                'title' => 'Artikel 1',
-                'author' => 'Reza Maulana Ismail',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, corrupti excepturi dolores non doloribus dignissimos facere in, maxime voluptates fuga ea nobis praesentium maiores. Minus cum harum facilis velit reprehenderit.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'artikel-2',
-                'title' => 'Artikel 2',
-                'author' => 'Reza Maulana Ismail',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque magni minus eius non nemo dolores sit, vel et repellendus repudiandae vitae recusandae explicabo eum ullam cupiditate laboriosam. Repudiandae, molestias consequuntur.'
-            ]
-            ];
-    }
-}
+use App\Models\Post;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
