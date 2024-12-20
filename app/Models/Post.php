c686d854454292621bd75a75ee2f1e8c9cee3669
kasurbantal <kasurbantal880@gmail.com>
2024-12-20 22:34:59 +0700

    feat: add 404 handling to Post::find() method
    - Updated the `find()` method in the `Post` class to return a 404 error if the requested slug is not found.
    - Ensured better error handling for non-existent posts.
    - Improved user experience by providing a clear response for invalid slugs.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,14 +22,13 @@ class Post {
             ];
     }
 
-    public static function find($slug)
+    public static function find($slug): array
     {
-        // //menggunakan callback function
-        // return Arr::first(static::all(), function ($post) use ($slug) {
-        //     return $post['slug'] == $slug;
-        // });
+        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
 
-        //menggunakan arrow function
-        return Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
+        if (! $post) {
+            abort(404);
+        }
+        return $post;
     }
 }
