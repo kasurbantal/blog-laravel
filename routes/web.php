838cce8f82db10d47b3eda523e0f728cafb6250c
kasurbantal <kasurbantal880@gmail.com>
2024-12-20 21:56:25 +0700

    refactor: move posts array into a dedicated Post class

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,27 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+class Post {
+    public static function all() {
+        return [
+            [
+                'id' => 1,
+                'slug' => 'artikel-1',
+                'title' => 'Artikel 1',
+                'author' => 'Reza Maulana Ismail',
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, corrupti excepturi dolores non doloribus dignissimos facere in, maxime voluptates fuga ea nobis praesentium maiores. Minus cum harum facilis velit reprehenderit.'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'artikel-2',
+                'title' => 'Artikel 2',
+                'author' => 'Reza Maulana Ismail',
+                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque magni minus eius non nemo dolores sit, vel et repellendus repudiandae vitae recusandae explicabo eum ullam cupiditate laboriosam. Repudiandae, molestias consequuntur.'
+            ]
+            ];
+    }
+}
+
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
@@ -16,46 +37,14 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'artikel-1',
-            'title' => 'Artikel 1',
-            'author' => 'Reza Maulana Ismail',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, corrupti excepturi dolores non doloribus dignissimos facere in, maxime voluptates fuga ea nobis praesentium maiores. Minus cum harum facilis velit reprehenderit.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'artikel-2',
-            'title' => 'Artikel 2',
-            'author' => 'Reza Maulana Ismail',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque magni minus eius non nemo dolores sit, vel et repellendus repudiandae vitae recusandae explicabo eum ullam cupiditate laboriosam. Repudiandae, molestias consequuntur.'
-        ]
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
+// route untuk menampilkan single post dalam blog
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'artikel-1',
-            'title' => 'Artikel 1',
-            'author' => 'Reza Maulana Ismail',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, corrupti excepturi dolores non doloribus dignissimos facere in, maxime voluptates fuga ea nobis praesentium maiores. Minus cum harum facilis velit reprehenderit.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'artikel-2',
-            'title' => 'Artikel 2',
-            'author' => 'Reza Maulana Ismail',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque magni minus eius non nemo dolores sit, vel et repellendus repudiandae vitae recusandae explicabo eum ullam cupiditate laboriosam. Repudiandae, molestias consequuntur.'
-        ]
-        ];
-
-        $post = Arr::first($posts, function ($post) use ($slug) {
+        $post = Arr::first(Post::all(), function ($post) use ($slug) {
             return $post['slug'] == $slug;
         });
-
         return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
