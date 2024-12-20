d2294c2ddba6ab346bb59080f9c21c211fda5698
kasurbantal <kasurbantal880@gmail.com>
2024-12-20 22:27:06 +0700

    refactor: replace callback function with arrow function in Post::find()

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -24,8 +24,12 @@ class Post {
 
     public static function find($slug)
     {
-        return Arr::first(static::all(), function ($post) use ($slug) {
-            return $post['slug'] == $slug;
-        });
+        // //menggunakan callback function
+        // return Arr::first(static::all(), function ($post) use ($slug) {
+        //     return $post['slug'] == $slug;
+        // });
+
+        //menggunakan arrow function
+        return Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
     }
 }
