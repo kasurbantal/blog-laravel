99b172391ea59950d41eb473719a0df255247033
kasurbantal <kasurbantal880@gmail.com>
2024-12-23 21:19:19 +0700

    feat: add fillable fields in Post model and display relative time in post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Models;
 
+use Faker\Provider\Lorem;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
@@ -9,4 +10,7 @@ class Post extends Model
     // protected $table  = 'nama_tabel'; //digunakan ketika nama tabel tidak default, misal model Post -> posts
     // protected $primaryKey  = 'nama_id'; //digunakan ketika nama primary key tidak default (id), misal post_id
 
+    //menunjukkan bahwa field tersebut boleh diisi secara manual
+    protected $fillable = ['slug','title', 'author', 'body'];
 }
+
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,12 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
-// route untuk menampilkan single post dalam blog dengan $slug
+// route untuk menampilkan single post dalam blog dengan $slug dengan metode Route Model Binding
+// Route Model Binding, secara otomatis mengambil data Post berdasarkan nilai slug tanpa perlu menggunakan find() secara manual.
+// Cara kerjanya adalah:
+// 1. Bagian {post:slug} memberi tahu Laravel untuk mencocokkan nilai dari parameter URL (slug) dengan kolom slug di model Post.
+// 2. Model Post harus memiliki slug yang unik untuk setiap entri agar ini berfungsi dengan benar.
+
 Route::get('/posts/{post:slug}', function (Post $post) {
 
         return view('post', ['title' => 'Single Post', 'post' => $post]);
