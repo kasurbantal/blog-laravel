8a0ba12a20cd1b201da986adaa8569b05b5db9e2
kasurbantal <kasurbantal880@gmail.com>
2024-12-23 21:33:09 +0700

    refactor: add comment in Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,16 +1,16 @@
 <?php
+
 namespace App\Models;
 
-use Faker\Provider\Lorem;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
 class Post extends Model
 {
+    // perintah di terminal untuk membuat model Post beserta migration-nya
+    // php artisan make:model Post -m
     // protected $table  = 'nama_tabel'; //digunakan ketika nama tabel tidak default, misal model Post -> posts
     // protected $primaryKey  = 'nama_id'; //digunakan ketika nama primary key tidak default (id), misal post_id
 
     //menunjukkan bahwa field tersebut boleh diisi secara manual
     protected $fillable = ['slug','title', 'author', 'body'];
 }
-
