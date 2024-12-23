2ea9cc2e2bcb71c128f5eed1d4b79fc65f76b11c
kasurbantal <kasurbantal880@gmail.com>
2024-12-23 20:30:27 +0700

    refactor: extend Post model from Eloquent Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,34 +1,12 @@
 <?php
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Post {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'artikel-1',
-                'title' => 'Artikel 1',
-                'author' => 'Reza Maulana Ismail',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, corrupti excepturi dolores non doloribus dignissimos facere in, maxime voluptates fuga ea nobis praesentium maiores. Minus cum harum facilis velit reprehenderit.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'artikel-2',
-                'title' => 'Artikel 2',
-                'author' => 'Reza Maulana Ismail',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque magni minus eius non nemo dolores sit, vel et repellendus repudiandae vitae recusandae explicabo eum ullam cupiditate laboriosam. Repudiandae, molestias consequuntur.'
-            ]
-            ];
-    }
+class Post extends Model
+{
+    // protected $table  = 'nama_tabel'; //digunakan ketika nama tabel tidak default, misal model Post -> posts
+    // protected $primaryKey  = 'nama_id'; //digunakan ketika nama primary key tidak default (id), misal post_id
 
-    public static function find($slug): array
-    {
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-
-        if (! $post) {
-            abort(404);
-        }
-        return $post;
-    }
 }
