d43a11fd1a462d49ce00c2efdbb4b97334dde77c
kasurbantal <kasurbantal880@gmail.com>
2025-01-02 16:48:53 +0700

    feat: install flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
     ],
     theme: {
         extend: {
@@ -18,5 +19,5 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [require("flowbite/plugin")],
 };
